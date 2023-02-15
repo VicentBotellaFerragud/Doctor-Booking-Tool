9c6a52c6b2dbac7095479bc62d33067f05ccdfda
Vicent Botella <vb@interap.de>
2023-02-15 11:39:16 +0100

    models created and registered

--- doctor_booking_tool/api/models.py
+++ doctor_booking_tool/api/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Doctor(models.Model):
+    title_choices = [
+        ('Dr.', 'Dr.'),
+        ('Prof. Dr.', 'Prof. Dr.'),
+        ('Dr. rer. nat.', 'Dr. rer. nat.')
+    ]
+    specialty_choices = [
+        ('Family medicine', 'Family medicine'),
+        ('Pediatrics', 'Pediatrics'),
+        ('Urology', 'Urology'),
+        ('Dermatology', 'Dermatology'),
+        ('Emergency medicine', 'Emergency medicine')
+    ]
+
+    title = models.CharField(
+        max_length=13, choices=title_choices, default='Dr.')
+    name = models.CharField(max_length=30)
+    specialty = models.CharField(
+        max_length=18, choices=specialty_choices, default='Family medicine')
+
+    def __str__(self):
+        return self.title + ' ' + self.name
+
+
+class Appointment(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=120)
+    date = models.DateField()
+    created_at = models.DateField(default=datetime.date.today)
+    patient = models.ForeignKey(User, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- doctor_booking_tool/user_auth/forms.py
+++ doctor_booking_tool/user_auth/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 
 class NewUserForm(UserCreationForm):
-
     class Meta:
         model = User
         fields = ("username", "password1", "password2",)
