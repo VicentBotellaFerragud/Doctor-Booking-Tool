ec9167481a55365a89b9964da40e598670488eb1
Vicent Botella <vb@interap.de>
2023-02-27 11:07:59 +0100

    formatting reviewed

--- doctor_booking_tool/user_auth/forms.py
+++ doctor_booking_tool/user_auth/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class NewUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ("username", "password1", "password2",)
+        fields = ('username', 'password1', 'password2',)
 
     def save(self):
         super(NewUserForm, self).save()
